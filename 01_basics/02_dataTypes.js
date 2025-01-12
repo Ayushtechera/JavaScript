0a365b6b5a8ab774addd95dbc98e87844d82ae3b
Ayush Kashyap <144578026+Ayushtechera@users.noreply.github.com>
2025-01-12 18:54:04 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // Treat all JS code as newer version of JS
+
+// alert(3+3) // We are using NodeJS not browser we can use alert in inspect>console window  
+
+// Diffferent types of dataTypes
+
+let name = "Ayush"
+let age = 21
+let isLoggedIn = false
+
+// More different types of DataTypes
+// number => range of number that it can contain is 2^53
+// bigint => is used to store big integer values 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => means value is undefined
+// symbol => we use symbols for uniqueness
+// object 
+
+console.log(typeof null); // Object
+console.log(typeof undefined); // give output undefined
